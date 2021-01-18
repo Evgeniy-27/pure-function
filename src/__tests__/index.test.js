6bf02ef0d154d4b462cc24180c11bb6d7da8323d
Evgeniy-27 <Li.Evgen@yandex.ru>
2021-01-18 23:40:10 +0700

    lint

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,41 +1,43 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable import/extensions */
 import health from '../index.js';
 
 test('health = 100', () => {
-  expect(health({'name': 'Маг', 'health': 100})).toBe('healthy');
+  expect(health({ name: 'Маг', health: 100 })).toBe('healthy');
 });
 
 test('health = 51', () => {
-  expect(health({'name': 'Маг', 'health': 51})).toBe('healthy');
+  expect(health({ name: 'Маг', health: 51 })).toBe('healthy');
 });
 
 test('health = 50', () => {
-  expect(health({'name': 'Маг', 'health': 50})).toBe('wounded');
+  expect(health({ name: 'Маг', health: 50 })).toBe('wounded');
 });
 
 test('health = 49', () => {
-  expect(health({'name': 'Маг', 'health': 49})).toBe('wounded');
+  expect(health({ name: 'Маг', health: 49 })).toBe('wounded');
 });
 
 test('health = 30', () => {
-  expect(health({'name': 'Маг', 'health': 30})).toBe('wounded');
+  expect(health({ name: 'Маг', health: 30 })).toBe('wounded');
 });
 
 test('health = 16', () => {
-  expect(health({'name': 'Маг', 'health': 16})).toBe('wounded');
+  expect(health({ name: 'Маг', health: 16 })).toBe('wounded');
 });
 
 test('health = 15', () => {
-  expect(health({'name': 'Маг', 'health': 15})).toBe('wounded');
+  expect(health({ name: 'Маг', health: 15 })).toBe('wounded');
 });
 
 test('health = 14', () => {
-  expect(health({'name': 'Маг', 'health': 14})).toBe('critical');
+  expect(health({ name: 'Маг', health: 14 })).toBe('critical');
 });
 
 test('health = 7', () => {
-  expect(health({'name': 'Маг', 'health': 7})).toBe('critical');
+  expect(health({ name: 'Маг', health: 7 })).toBe('critical');
 });
 
 test('health = 0', () => {
-  expect(health({'name': 'Маг', 'health': 0})).toBe('critical');
+  expect(health({ name: 'Маг', health: 0 })).toBe('critical');
 });
