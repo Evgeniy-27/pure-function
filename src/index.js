4f5e02c9f96d2fea32fa51c454219a47efa8b297
Evgeniy-27 <Li.Evgen@yandex.ru>
2021-01-18 23:55:26 +0700

    test

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-export default function health({'name': name, 'health': health}) {
-    if(health > 50) {
-        return 'healthy';
-    };
+/* eslint-disable no-shadow */
+/* eslint-disable no-unused-vars */
+/* eslint-disable consistent-return */
+export default function health({ name, health }) {
+  if (health > 50) {
+    return 'healthy';
+  }
 
-    if((health >= 15) && (health <= 50)) {
-        return 'wounded';
-    };
+  if ((health >= 15) && (health <= 50)) {
+    return 'wounded';
+  }
 
-    if(health < 15) {
-        return 'critical';
-    };
+  if (health < 15) {
+    return 'critical';
+  }
 }
